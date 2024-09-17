02be70359f41ee4328662d6762b88780ceb2885f
Anderson-Armando <arevalosanderson9@gmail.com>
2024-09-17 12:40:24 -0300

    Criação da API de Maps

---
+++ PAM2-Maps/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PAM2_Maps
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PAM2-Maps/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Maps;
+
+namespace PAM2_Maps
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var Location = new Location(-23.51972836623556, -46.59645381774267);
+            var mapSpan = new MapSpan(Location, 0.01, 0.01);
+            map.MoveToRegion(mapSpan);
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
